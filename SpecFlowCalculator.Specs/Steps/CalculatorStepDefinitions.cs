84c617d825e510469c81ac88f6bdb7f736fee1f1
StevanFreeborn <sfreeborn7@gmail.com>
2021-08-17 23:37:41 -0500

    Removed comment from CalculatorStepDefinition.cs

--- SpecFlowCalculator.Specs/Steps/CalculatorStepDefinitions.cs
+++ SpecFlowCalculator.Specs/Steps/CalculatorStepDefinitions.cs
@@ -6,9 +6,6 @@ namespace SpecFlowCalculator.Specs.Steps
     [Binding]
     public sealed class CalculatorStepDefinitions
     {
-
-        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
-
         private readonly ScenarioContext _scenarioContext;
 
         private readonly Calculator _calculator = new Calculator();
